228eb124565aed146f2b438ff4704a5472fc258d
GokeOne <160627194+GokeOne@users.noreply.github.com>
2024-05-17 03:48:06 -0400

    Add files via upload

--- SySenumeration.sh
+++ SySenumeration.sh
@@ -6,7 +6,7 @@ line=$(printf "%0.s-" {1..50})
 
 write_report() {
         local message=$1
-        echo "$message" >> $report
+        echo -e "$message" >> $report
 }
 line() {
 	echo "$line" >>$report
@@ -17,19 +17,21 @@ enumerate() {
 	perm=$(ls -l /etc/passwd | awk '{print $1}' | cut -c 8-10)
 	if [ $perm == "r--"  ] || [ $perm == "r-x" ] || [ $perm == "rw-" ] || [ $perm == "rwx" ]; then
 		local etcpass=$(cat /etc/passwd)
-		write_report="We have read permissions"
+		write_report "\033[34m\033[1m System users:  \033[0m"
+		line
 		write_report "$etcpass"
 		line
 	else
+		line
 		local message2="/etc/passwd don't have read permissions"
 		write_report "$message2"
 		line
 	fi
-	kernelName="Kernel name: $(uname -s)"
-	kernelRelease="Kernel release: $(uname -r)"
-	kernelVersion="Kernel version: $(uname -v)"
-	machineArch="Architecture: $(uname -m)"
-	os="Operating System: $(uname -o)"
+	kernelName="\033[34m\033[1m Kernel name:\033[0m $(uname -s)"
+	kernelRelease="\033[34m\033[1m Kernel release:\033[0m $(uname -r)"
+	kernelVersion="\033[34m\033[1m Kernel version:\033[0m $(uname -v)"
+	machineArch="\033[34m\033[1m Architecture:\033[0m $(uname -m)"
+	os="\033[34m\033[1m Operating System:\033[0m $(uname -o)"
 
 	write_report "$kernelName"
 	line
@@ -42,31 +44,31 @@ enumerate() {
 	write_report "$os"
 	line
 
-	write_report "System info"
+	write_report "\033[34m\033[1m System info\033[0m"
 	line
 	etcRelease="$(cat /etc/*release)"
 	write_report "$etcRelease"
 	line
-	write_report "Disk information"
+	write_report "\033[34m\033[1m Disk information \033[0m"
 	line
 	diskinf=$(df -h)
 	write_report "$diskinf"
 	line
 
-	write_report "Installed kernels"
+	write_report "\033[34m\033[1m Installed kernels \033[0m"
 	line
 
 	inskernel=$(dpkg -l linux-image-*)
 	write_report "$inskernel"
 	line
 
-	write_report "Arp table"
+	write_report "\033[34m\033[1m Arp table \033[0m"
 	line
 	arptable=$(arp -e)
 	write_report "$arptable"
 	line
 
-	write_report "User info"
+	write_report "\033[34m\033[1m User info \033[0m"
 	line
 	who=$(whoami)
 	write_report "Whoami: $who"
@@ -75,12 +77,12 @@ enumerate() {
 	write_report "ID: $(id)"
 	line
 
-	write_report "Last connections"
+	write_report "\033[34m\033[1m Last connections \033[0m"
 	line
 	write_report "$(last)"
 	line
 
-	write_report "Open port connections"
+	write_report "\033[34m\033[1m Open port connections \033[0m"
 	line
 	write_report "$(netstat -tuln)"
 	line
